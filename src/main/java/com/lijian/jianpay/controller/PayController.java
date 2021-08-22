1009739f3abeb7577e531004d28e0cdb399a70c0
lijian <ljhhh@qq.com>
2021-08-22 11:26:49 +0800

    支付成功后，微信接口回调本地接口

--- src/main/java/com/lijian/jianpay/controller/PayController.java
+++ src/main/java/com/lijian/jianpay/controller/PayController.java
@@ -5,9 +5,7 @@ import com.lly835.bestpay.model.PayResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
@@ -15,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Slf4j
 @RequestMapping("/pay")
 public class PayController {
 
@@ -32,5 +31,9 @@ public class PayController {
         return new ModelAndView("create", map);
     }
 
+    @PostMapping("notify")
+    public void asyncNotify(@RequestBody String notifyData){
+        log.info("notifyDta={}",notifyData);
+    }
 
 }
--- src/main/java/com/lijian/jianpay/service/impl/PayService.java
+++ src/main/java/com/lijian/jianpay/service/impl/PayService.java
@@ -20,7 +20,7 @@ public class PayService implements IPayService {
         wxPayConfig.setAppId("wxd898fcb01713c658");
         wxPayConfig.setMchId("1483469312");
         wxPayConfig.setMchKey("7mdApPMfXddfWWbbP4DUaVYm2wjyh3v3");
-        wxPayConfig.setNotifyUrl("http://127.0.0.1");
+        wxPayConfig.setNotifyUrl("http://betterfly.natapp1.cc/pay/notify");
 
         //支付类, 所有方法都在这个类里
         BestPayServiceImpl bestPayService = new BestPayServiceImpl();
